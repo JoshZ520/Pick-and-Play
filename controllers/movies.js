ba3a06b302db45126d231a13256169e802320074
Cabe-Lee <cabetlee@gmail.com>
2026-03-13 14:16:57 -0700

    Refactor movie controller methods for improved error handling and code clarity

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,52 +1,32 @@
-const mongodb = require('../DB/connect');
+const { getDb } = require('../DB/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const allMovies = async(req, res, next) => {
+const allMovies = async (req, res, next) => {
     try {
-        const result = await mongodb.getDB().collection('movies').find();
-        result.toArray((err, lists) => {
-            if (err) {
-                res.status(400).json({message:err});
-            }
-        })
-        .then((lists)=> {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
-    } catch(err) {
-        res.status(500).json(err.message || 'Some error occured while getting the list of movies');
+    const lists = await getDb().collection('movies').find().toArray();
+        res.status(200).json(lists);
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Some error occurred while getting the list of movies' });
     }
 };
 
-const singleMovie = async(req, res, next) => {
+const singleMovie = async (req, res, next) => {
     if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json({error: "Must be a valid movie id to find a movie."});
+        return res.status(400).json({ error: "Must use a valid movie id" });
     }
     const movieId = new ObjectId(req.params.id);
     try {
-        const result = await mongodb
-            .getDB()
-            .collection('movies')
-            .find({_id: movieId});
-        result.toArray((err, lists) => {
-            if(err) {
-                res.status(400).json({message:err});
-            }
-        })
-        .then((lists) => {
-            if(!lists[0]) {
-                return res.status(404).json({ error: "Movie not found"});
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists[0]);
-        });
-    } catch(error) {
-        console.log(error.message);
-        next(error);
+    const lists = await getDb().collection('movies').find({ _id: movieId }).toArray();
+        if (!lists.length) {
+            return res.status(404).json({ error: "Movie not found" });
+        }
+        res.status(200).json(lists[0]);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
-const createNewMovie = async(req, res, next) => {
+const createNewMovie = async (req, res, next) => {
     try {
         const newMovie = {
             title: req.body.title,
@@ -56,62 +36,56 @@ const createNewMovie = async(req, res, next) => {
             description: req.body.description,
             imageURL: req.body.imageURL
         };
-
-        const result = await mongodb.getDB().collection('movies').insertOne(newMovie);
-        if(result.acknowledged) {
+const result = await getDb().collection('movies').insertOne(newMovie);
+        if (result.acknowledged) {
             res.status(201).json(result);
-            console.log('It worked!!!!');
         } else {
-            res.status(500).json(result.err || 'Some error occured while making the movie');
+            res.status(500).json({ message: 'Some error occurred while creating the movie' });
         }
-    } catch(err) {
-        res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
-const updateMovie = async(req, res, next) => {
+const updateMovie = async (req, res, next) => {
     if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json({error: "Must be a valid movie id to find a movie."});
+        return res.status(400).json({ error: "Must use a valid movie id" });
     }
-
+    const movieId = new ObjectId(req.params.id);
     try {
-        const movieId = new ObjectId(req.params.id);
         const updateMovie = {
             title: req.body.title,
             genre: req.body.genre,
             rating: req.body.rating,
-            runTime: req.body.runtime,
+            runtime: req.body.runtime,
             description: req.body.description,
             imageURL: req.body.imageURL
         };
-        const result = await mongodb.getDB().collection('movies').replaceOne({_id: movieId}, updateMovie);
-        console.log(result);
+const result = await getDb().collection('movies').replaceOne({ _id: movieId }, updateMovie);
         if (result.modifiedCount > 0) {
             res.status(204).send();
         } else {
-            res.status(500).json(result.error || 'Some error occurred while updating the movie');
+            res.status(404).json({ message: 'No movie found to update' });
         }
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json({ message: err.message });
     }
 };
 
-const deleteMovie = async(req, res, next) => {
+const deleteMovie = async (req, res, next) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ error: "Must use a valid movie id" });
+    }
+    const movieId = new ObjectId(req.params.id);
     try {
-        if (!ObjectId.isValid(req.params.id)) {
-            res.status(400).json({error: "Must be a valid movie id to find a movie."});
-        }
-
-        const movieId = new ObjectId(req.params.id);
-        const result = await mongodb.getDB().collection('movies').deleteOne({_id: movieId});
-        console.log(result);
+const result = await getDb().collection('movies').deleteOne({ _id: movieId });
         if (result.deletedCount > 0) {
             res.status(200).send();
         } else {
-            res.status(500).json(result.error || 'Some error occurred while deleting the movie');
+            res.status(404).json({ message: 'No movie found to delete' });
         }
-    } catch(err) {
-        res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 };
 
