55362f061dcebb6dfb914c877e6ee55716e160ce
Josh <joshuazob@gmail.com>
2026-03-19 22:03:00 -0600

    updated login to work with GoogleOAuth

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+
+// #swagger.ignore = true
 router.use('/api-docs', swaggerUi.serve);
+
+// #swagger.ignore = true
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
